4c5c613f3b1b1430249eff355b29e77adf361654
nodes11 <desmarks11@gmail.com>
2016-10-23 23:20:27 -0700

    Uses webcam to watch room.

---
+++ watch.py
@@ -0,0 +1,43 @@
+import StringIO
+import subprocess
+import os
+import time
+from playsound import playsound
+from datetime import datetime
+from PIL import Image
+
+threshold = 10
+sensitivity = 60000
+evenCapture = False
+
+pixels1 = None
+pixels2 = None
+
+def getFrame():
+    command = "sudo fswebcam output.png"
+    imageData = StringIO.StringIO()
+    imageData.write(subprocess.check_output(command, shell=True))
+
+while (True):
+    getFrame()
+
+    if (evenCapture is False):
+        im1 = Image.open("output.png")
+        pixels1 = list(im1.getdata())
+    elif (evenCapture is True):
+        im2 = Image.open("output.png")
+        pixels2 = list(im2.getdata())
+
+    # Count changed pixels
+    changedPixels = 0
+    print(len(pixels1))
+    if (pixels1 is not None and pixels2 is not None): 
+        for i in range(len(pixels1)):
+            if (abs(sum(pixels1[i]) - sum(pixels2[i])) > 8):
+                changedPixels = changedPixels + 1
+
+    	if (changedPixels > sensitivity):
+            subprocess.call(["aplay", "sound.wav"])    
+
+    time.sleep(.5)
+    evenCapture =  not evenCapture
