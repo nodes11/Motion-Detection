ea9bd8301280b3f175dbf6aa792c5968eb478c45
nodes11 <desmarks11@gmail.com>
2016-10-23 23:56:57 -0700

    Clean up

--- watch.py
+++ watch.py
@@ -1,8 +1,6 @@
-import StringIO
 import subprocess
 import os
 import time
-from playsound import playsound
 from datetime import datetime
 from PIL import Image
 
@@ -14,9 +12,8 @@ pixels1 = None
 pixels2 = None
 
 def getFrame():
-    command = "sudo fswebcam output.png"
-    imageData = StringIO.StringIO()
-    imageData.write(subprocess.check_output(command, shell=True))
+    command = "sudo fswebcam -q output.png"
+    subprocess.check_output(command, shell=True)
 
 while (True):
     getFrame()
@@ -27,17 +24,16 @@ while (True):
     elif (evenCapture is True):
         im2 = Image.open("output.png")
         pixels2 = list(im2.getdata())
-
-    # Count changed pixels
-    changedPixels = 0
-    print(len(pixels1))
+    
+    changedPixels= 0
     if (pixels1 is not None and pixels2 is not None): 
         for i in range(len(pixels1)):
             if (abs(sum(pixels1[i]) - sum(pixels2[i])) > 8):
                 changedPixels = changedPixels + 1
 
     	if (changedPixels > sensitivity):
-            subprocess.call(["aplay", "sound.wav"])    
+	    print("farting...")
+            subprocess.call(["aplay", "fart-1.wav"])    
 
     time.sleep(.5)
-    evenCapture =  not evenCapture
+    evenCapture = not evenCapture
